9d2d4d291f515ae4f5a30e4487c9d34c220d431e
Henry Rennell <henryrennell@gmail.com>
2023-09-28 19:26:28 +1000

    more updates

--- db/index.js
+++ db/index.js
@@ -1,20 +1,32 @@
 const { Pool } = require('pg')
 require('dotenv').config();
-let pool;
 
-if(process.env.NODE_MODE === 'production') {
-    pool = new Pool({
+
+// if(process.env.NODE_MODE === 'production') {
+//     pool = new Pool({
+//         database: process.env.DATABASE,
+//         user: process.env.USER,
+//         password: process.env.PASSWORD
+//     });
+
+// } else if (process.env.NODE_MODE === 'development') {
+//     pool = new Pool({
+//         connectionString: process.env.CONNECTION_STRING
+//     });
+// }
+
+const config = {
+    dev: {
         database: process.env.DATABASE,
         user: process.env.USER,
         password: process.env.PASSWORD
-    });
+    }, 
 
-} else if (process.env.NODE_MODE === 'development') {
-    pool = new Pool({
+    prod: {
         connectionString: process.env.CONNECTION_STRING
-    });
+    }
 }
 
-
+const pool = new Pool(process.env.NODE_MODE === 'production'? config.prod : config.dev);
 
 module.exports = pool;
